5d676c0d8b153ac4dcde162f4698ae1c6618c878
dgran <dgranh@gmail.com>
2021-01-14 03:39:02 +0100

    cookies policy dark mode

--- resources/views/cookies-policy.blade.php
+++ resources/views/cookies-policy.blade.php
@@ -6,17 +6,17 @@
     </x-slot>
 
     	<div class="max-w-7xl mx-auto md:px-1 sm:px-3 md:px-6 lg:px-8 md:my-8 flex border-t border-gray-200 dark:border-gray-700 md:border-transparent">
-            <div class="bg-white dark:bg-gray-900 overflow-hidden md:shadow-md md:rounded-md md:mx-0 p-6">
+            <div class="bg-white dark:bg-gray-750 overflow-hidden md:shadow-md md:rounded-md md:mx-0 p-6 text-gray-900 dark:text-gray-200">
                 <p>
                     Una cookie es un archivo de texto que se almacena en el ordenador o dispositivo móvil mediante un servidor Web y tan solo ese servidor será capaz de recuperar o leer el contenido de la cookie y permiten al Sitio Web recordar preferencias de navegación y navegar de manera eficiente. Las cookies hacen la interacción entre el usuario y el sitio Web más rápida y fácil.
                 </p>
-                <h4 class="text-pretty-red py-4 font-bold text-xl">
+                <h4 class="text-pretty-red dark:text-white py-4 font-bold text-xl">
                 	Información general
                 </h4>
                 <p>
 					Está página Web utiliza cookies. Las cookies son pequeños archivos de texto generados por las páginas web que usted visita, las cuales contienen los datos de sesión que pueden ser de utilidad más adelante en la página web. De esta forma esta Web recuerda información sobre su visita, lo que puede facilitar su próxima visita y hacer que el sitio Web le resulte más útil.
                 </p>
-                <h4 class="text-pretty-red py-4 font-bold text-xl">
+                <h4 class="text-pretty-red dark:text-white py-4 font-bold text-xl">
                 	¿Cómo funcionan las Cookies?
                 </h4>
                 <p>
@@ -26,14 +26,14 @@
 					Los datos permiten que esta Web pueda mantener su información entre las páginas, y también para analizar la forma de interactuar con el sitio Web. Las cookies son seguras ya que sólo pueden almacenar la información que se puso en su lugar por el navegador, lo que es información que el usuario ha introducido en el navegador o la que se incluye en la solicitud de página. No puede ejecutar el código y no se puede utilizar para acceder a su ordenador. Si una página web cifra la información de la cookie, sólo la página web puede leer la información.
                 </p>
 
-                <h3 class="text-2xl py-4 font-bold">
+                <h3 class="text-2xl dark:text-white py-4 font-bold">
                 	¿Qué tipos de cookies utilizamos?
                 </h3>
                 <p>
                 	Las cookies que utiliza esta página Web se pueden distinguir según los siguientes criterios:
                 </p>
 
-                <h4 class="text-pretty-red py-4 font-bold text-xl">
+                <h4 class="text-pretty-red dark:text-white py-4 font-bold text-xl">
                 	1. Tipos de cookies según la entidad que las gestiona:
                 </h4>
                 <p>
@@ -41,17 +41,17 @@
                 </p>
                 <ul class="py-2 list-disc list-inside">
                 	<li class="py-1">
-                		<strong>Cookies propias:</strong> Son aquéllas que se envían al equipo terminal del usuario desde un equipo o dominio gestionado por el propio editor y desde el que se presta el servicio solicitado por el usuario.
+                		<strong class="dark:text-white">Cookies propias:</strong> Son aquéllas que se envían al equipo terminal del usuario desde un equipo o dominio gestionado por el propio editor y desde el que se presta el servicio solicitado por el usuario.
                 	</li>
                 	<li class="py-1">
-                		<strong>Cookies de terceros:</strong> Son aquéllas que se envían al equipo terminal del usuario desde un equipo o dominio que no es gestionado por el editor, sino por otra entidad que trata los datos obtenidos través de las cookies.
+                		<strong class="dark:text-white">Cookies de terceros:</strong> Son aquéllas que se envían al equipo terminal del usuario desde un equipo o dominio que no es gestionado por el editor, sino por otra entidad que trata los datos obtenidos través de las cookies.
                 	</li>
                 </ul>
                 <p>
                 	En el caso de que las cookies sean instaladas desde un equipo o dominio gestionado por el propio editor pero la información que se recoja mediante éstas sea gestionada por un tercero, no pueden ser consideradas como cookies propias.
                 </p>
 
-                <h4 class="text-pretty-red py-4 font-bold text-xl">
+                <h4 class="text-pretty-red dark:text-white py-4 font-bold text-xl">
                 	2. Tipos de cookies según el plazo de tiempo que permanecen activadas:
                 </h4>
                 <p>
@@ -59,14 +59,14 @@
                 </p>
                 <ul class="py-2 list-disc list-inside">
                 	<li class="py-1">
-                		<strong>Cookies de sesión:</strong> Son un tipo de cookies diseñadas para recabar y almacenar datos mientras el usuario accede a una página web. Se suelen emplear para almacenar información que solo interesa conservar para la prestación del servicio solicitado por el usuario en una sola ocasión (p.e. una lista de productos adquiridos).
+                		<strong class="dark:text-white">Cookies de sesión:</strong> Son un tipo de cookies diseñadas para recabar y almacenar datos mientras el usuario accede a una página web. Se suelen emplear para almacenar información que solo interesa conservar para la prestación del servicio solicitado por el usuario en una sola ocasión (p.e. una lista de productos adquiridos).
                 	</li>
                 	<li class="py-1">
-                		<strong>Cookies persistentes:</strong> Son un tipo de cookies en el que los datos siguen almacenados en el terminal y pueden ser accedidos y tratados durante un periodo definido por el responsable de la cookie, y que puede ir de unos minutos a varios años.
+                		<strong class="dark:text-white">Cookies persistentes:</strong> Son un tipo de cookies en el que los datos siguen almacenados en el terminal y pueden ser accedidos y tratados durante un periodo definido por el responsable de la cookie, y que puede ir de unos minutos a varios años.
                 	</li>
                 </ul>
 
-                <h4 class="text-pretty-red py-4 font-bold text-xl">
+                <h4 class="text-pretty-red dark:text-white py-4 font-bold text-xl">
                 	3. Tipos de cookies según su finalidad:
                 </h4>
                 <p>
@@ -74,17 +74,17 @@
                 </p>
                 <ul class="py-2 list-disc list-inside">
                 	<li class="py-1">
-                		<strong>Cookies técnicas:</strong> Son aquéllas que permiten al usuario la navegación a través de una página web, plataforma o aplicación y la utilización de las diferentes opciones o servicios que en ella existan como, por ejemplo, controlar el tráfico y la comunicación de datos, identificar la sesión, acceder a partes de acceso restringido, recordar los elementos que integran un pedido, realizar el proceso de compra de un pedido, realizar la solicitud de inscripción o participación en un evento, utilizar elementos de seguridad durante la navegación, almacenar contenidos para la difusión de vídeos o sonido o compartir contenidos a través de redes sociales.
+                		<strong class="dark:text-white">Cookies técnicas:</strong> Son aquéllas que permiten al usuario la navegación a través de una página web, plataforma o aplicación y la utilización de las diferentes opciones o servicios que en ella existan como, por ejemplo, controlar el tráfico y la comunicación de datos, identificar la sesión, acceder a partes de acceso restringido, recordar los elementos que integran un pedido, realizar el proceso de compra de un pedido, realizar la solicitud de inscripción o participación en un evento, utilizar elementos de seguridad durante la navegación, almacenar contenidos para la difusión de vídeos o sonido o compartir contenidos a través de redes sociales.
                 	</li>
                 	<li class="py-1">
-                		<strong>Cookies de personalización:</strong> Son aquéllas que permiten al usuario acceder al servicio con algunas características de carácter general predefinidas en función de una serie de criterios en el terminal del usuario como por ejemplo serian el idioma, el tipo de navegador a través del cual accede al servicio, la configuración regional desde donde accede al servicio, etc.
+                		<strong class="dark:text-white">Cookies de personalización:</strong> Son aquéllas que permiten al usuario acceder al servicio con algunas características de carácter general predefinidas en función de una serie de criterios en el terminal del usuario como por ejemplo serian el idioma, el tipo de navegador a través del cual accede al servicio, la configuración regional desde donde accede al servicio, etc.
                 	</li>
                 	<li class="py-1">
-                		<strong>Cookies de análisis:</strong> Son aquéllas que permiten al responsable de las mismas, el seguimiento y análisis del comportamiento de los usuarios de los sitios web a los que están vinculadas. La información recogida mediante este tipo de cookies se utiliza en la medición de la actividad de los sitios web, aplicación o plataforma y para la elaboración de perfiles de navegación de los usuarios de dichos sitios, aplicaciones y plataformas, con el fin de introducir mejoras en función del análisis de los datos de uso que hacen los usuarios del servicio.
+                		<strong class="dark:text-white">Cookies de análisis:</strong> Son aquéllas que permiten al responsable de las mismas, el seguimiento y análisis del comportamiento de los usuarios de los sitios web a los que están vinculadas. La información recogida mediante este tipo de cookies se utiliza en la medición de la actividad de los sitios web, aplicación o plataforma y para la elaboración de perfiles de navegación de los usuarios de dichos sitios, aplicaciones y plataformas, con el fin de introducir mejoras en función del análisis de los datos de uso que hacen los usuarios del servicio.
                 	</li>
                 </ul>
 
-                <h3 class="text-2xl py-4 font-bold">
+                <h3 class="text-2xl dark:text-white py-4 font-bold">
                 	Cómo gestionar las Cookies en su equipo: la desactivación y eliminación de las cookies
                 </h3>
                 <p>
@@ -114,7 +114,7 @@
 					A través de su navegador, usted también puede ver las cookies que están en su ordenador, y borrarlas según crea conveniente. Las cookies son archivos de texto, los puede abrir y leer el contenido. Los datos dentro de ellos casi siempre está cifrado con una clave numérica que corresponde a una sesión en Internet por lo que muchas veces no tiene sentido más allá que la página web que lo escribió.
                 </p>
 
-                <h3 class="text-2xl py-4 font-bold">
+                <h3 class="text-2xl dark:text-white py-4 font-bold">
                 	Consentimiento informado
                 </h3>
                 <p>
