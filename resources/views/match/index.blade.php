f5266151f292354b75d6c075e451d3ecb58c02ff
dgran <dgranh@gmail.com>
2021-02-03 15:55:43 +0100

    change data order in match view, match played

--- resources/views/match/index.blade.php
+++ resources/views/match/index.blade.php
@@ -19,6 +19,7 @@
 			@endif
 
 			@if ($match->played())
+				@include('match.boxscore')
 				@include('match.match_info')
 				<div class="grid grid-cols-1 md:grid-cols-2 gap-6 my-6">
 					<div class="bg-white dark:bg-gray-750 border border-gray-150 dark:border-transparent shadow-md rounded mx-3 md:mx-0">
@@ -28,7 +29,6 @@
 						@include('match.game_tops')
 					</div>
 				</div>
-				@include('match.boxscore')
 			@endif
 		</div>
 	</div>
