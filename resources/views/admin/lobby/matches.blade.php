5b91146ef48cac1c299279fc447f1ac35863cff6
David Gran Herrer <dgran@MBP-de-David.home>
2021-12-06 12:12:29 +0100

    working in admin lobby - fix empty data

--- resources/views/admin/lobby/matches.blade.php
+++ resources/views/admin/lobby/matches.blade.php
@@ -1,33 +1,37 @@
 <div>
-    <h4 class="text-lg font-semibold mb-1.5 | pb-1.5">Partidos disponibles ({{ $matches->count() }})</h4>
-    <ul class="text-sm | space-y-2.5">
-        @foreach ($matches as $match)
-            <li class="bg-white dark:bg-gray-750 | border border-gray-200 dark:border-gray-700 rounded | py-1.5 | hover:border-blue-400 dark:hover:border-dark-link | select-none">
-                <a href="{{ route('match', $match->id) }}">
-                    <div class="flex flex-col items-center">
-                        <p class="text-xs">
-                            ID: {{ $match->id }}
-                        </p>
-                        <div class="flex items-center">
-                            <div class="flex-1 flex items-center justify-end">
-                                <div class="flex flex-col items-end">
-                                    <span>{{ $match->localTeam->team->medium_name }}</span>
-                                    <span class="text-xs text-gray-400">{{ $match->localTeam->team->user->name }}</span>
+    <h4 class="text-lg font-semibold mb-2.5">Partidos disponibles ({{ $matches->count() }})</h4>
+    @if ($matches->count() > 0)
+        <ul class="text-sm | space-y-2.5">
+            @foreach ($matches as $match)
+                <li class="bg-white dark:bg-gray-750 | border border-gray-200 dark:border-gray-700 rounded | py-1.5 | hover:border-blue-400 dark:hover:border-dark-link | select-none">
+                    <a href="{{ route('match', $match->id) }}">
+                        <div class="flex flex-col items-center">
+                            <p class="text-xs">
+                                ID: {{ $match->id }}
+                            </p>
+                            <div class="flex items-center">
+                                <div class="flex-1 flex items-center justify-end">
+                                    <div class="flex flex-col items-end">
+                                        <span>{{ $match->localTeam->team->medium_name }}</span>
+                                        <span class="text-xs text-gray-400">{{ $match->localTeam->team->user->name }}</span>
+                                    </div>
+                                    <img src="{{ $match->localTeam->team->getImg() }}" alt="{{ $match->localTeam->team->medium_name }}" class="w-12 h-12 object-cover | ml-1.5">
                                 </div>
-                                <img src="{{ $match->localTeam->team->getImg() }}" alt="{{ $match->localTeam->team->medium_name }}" class="w-12 h-12 object-cover | ml-1.5">
-                            </div>
-                            <div class="flex-0 w-12 text-center | text-2xl font-bold">Vs</div>
-                            <div class="flex-1 flex items-center justify-start">
-                                <img src="{{ $match->visitorTeam->team->getImg() }}" alt="{{ $match->visitorTeam->team->medium_name }}" class="w-12 h-12 object-cover | mr-1.5">
-                                <div class="flex flex-col items-start">
-                                    <span>{{ $match->visitorTeam->team->medium_name }}</span>
-                                    <span class="text-xs text-gray-400">{{ $match->visitorTeam->team->user->name }}</span>
+                                <div class="flex-0 w-12 text-center | text-2xl font-bold">Vs</div>
+                                <div class="flex-1 flex items-center justify-start">
+                                    <img src="{{ $match->visitorTeam->team->getImg() }}" alt="{{ $match->visitorTeam->team->medium_name }}" class="w-12 h-12 object-cover | mr-1.5">
+                                    <div class="flex flex-col items-start">
+                                        <span>{{ $match->visitorTeam->team->medium_name }}</span>
+                                        <span class="text-xs text-gray-400">{{ $match->visitorTeam->team->user->name }}</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-            </li>
-        @endforeach
-    </ul>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    @else
+        <div class="bg-white dark:bg-gray-750 | border border-gray-200 dark:border-gray-700 | p-4 | text-center | text-sm">No hay partidos disponibles...</div>
+    @endif
 </div>
