bc09acfa4d7b05811974c4e63245f89c5a134ae6
dgran <dgranh@gmail.com>
2021-12-31 05:36:10 +0100

    add work in progress mark in team views

--- resources/views/teams/team/index.blade.php
+++ resources/views/teams/team/index.blade.php
@@ -2,6 +2,7 @@
 
 	<div class="max-w-7xl mx-auto sm:px-3 sm:px-6 lg:px-8 my-4 md:my-8">
 
+		<h4 class="pb-3 text-pretty-red text-2xl">Work in progress...</h4>
 		@include('teams.team.menu')
 
 		@switch($op)
