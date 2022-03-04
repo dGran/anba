5b40d58444c228dd1e8eb9b0f8117f53c133c84e
David Gran Herrer <dgran@MBP-de-David.home>
2022-03-05 00:12:31 +0100

    change data for testing view

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,7 +9,7 @@ class TestController extends Controller
     public function index() {
         $usersOnline = \App\Models\Session::whereNotNull('user_id')->distinct('user_id')->get();
         $visitorsOnline = \App\Models\Session::select('ip_address')->whereNull('user_id')->distinct('ip_address')->get();
-        dd($visitorsOnline);
+        dd($usersOnline);
         // view()->share('usersOnline', $usersOnline);
     }
 }
