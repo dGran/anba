d24331989a95548981ffaec3911265ed096cd9ef
dgrandev <dgranh@gmail.com>
2024-06-28 02:06:27 +0200

    [IMP] global refactoring and code improvement

--- resources/views/admin/admin_logs/table.blade.php
+++ resources/views/admin/admin_logs/table.blade.php
@@ -1,3 +1,5 @@
+@php use App\Enum\TableFilters; @endphp
+
 <div class="admin-crud-table-wrapper shadow-sm mt-2">
 	@if ($data->count()>0)
 		<table class="admin-crud-table {{ !$isFixedFirstColumn ?: 'fixed-first' }} {{ !$isShowStriped ?: 'striped' }}">
@@ -7,7 +9,14 @@
 	@else
 		<div class="p-3">
 			No existen resultados
-			@if ($search || $user !== "all" || $type !== "all" || $table !== "all" || $perPage !== "25")
+
+			@if (
+                $search !== TableFilters::VALUE_NULL_STRING
+                || $user !== TableFilters::VALUE_ALL
+                || $type !== TableFilters::VALUE_ALL
+                || $table !== TableFilters::VALUE_ALL
+                || $perPage !== TableFilters::PER_PAGE_DEFAULT_VALUE
+            )
 				con los filtros aplicados
 			@endif
 		</div>
