a1b45178be735e37357f9c2bebb31d9d1aa661a7
dgran <dgranh@gmail.com>
2024-11-03 00:07:10 +0100

    Send discord notification optimization

--- app/Listeners/SendPostNotification.php
+++ app/Listeners/SendPostNotification.php
@@ -3,80 +3,91 @@
 namespace App\Listeners;
 
 use App\Events\PostStored;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Log;
 
 class SendPostNotification
 {
-	public $active_notifications = true;
-	public $test_mode = false;
+	public $activeNotifications = true;
+	public $testMode = false;
 
     public function __construct()
     {
     	$config = \App\Models\Config::first();
+
     	if ($config) {
-    		$this->active_notifications = $config->active_notifications;
-    		$this->test_mode = $config->notifications_test_mode;
+    		$this->activeNotifications = $config->active_notifications;
+    		$this->testMode = $config->notifications_test_mode;
     	}
+
+        $this->httpClient = new Client([
+            'on_stats' => function (\GuzzleHttp\TransferStats $stats) {
+                Log::info('HTTP Transfer Stats:', [
+                    'total_time' => $stats->getTransferTime(),
+                    'request_size' => $stats->getHandlerStats()['request_size'] ?? null,
+                    'response_size' => $stats->getHandlerStats()['size_download'] ?? null,
+                    'status_code' => $stats->getHandlerStats()['http_code'] ?? null,
+                ]);
+            }
+        ]);
     }
 
-    public function handle(PostStored $event)
+    /**
+     * @throws GuzzleException
+     */
+    public function handle(PostStored $event): void
     {
-    	if ($this->active_notifications) {
-    		if ($this->test_mode) {
-    			$webhook = config('discord.webhook_dev');
-    		} else {
-		    	switch ($event->post->type) {
-		    		case 'general':
-		    			$webhook = config('discord.webhook_general');
-		    			break;
-		    		case ($event->post->type == 'resultados' || $event->post->type == 'rachas' || $event->post->type == 'destacados'):
-		    			$webhook = config('discord.webhook_matches');
-		    			break;
-		    		case 'records':
-		    			$webhook = config('discord.webhook_records');
-		    			break;
-		    		case 'lesiones':
-		    			$webhook = config('discord.webhook_injuries');
-		    			break;
-		    		case 'movimientos':
-		    			$webhook = config('discord.webhook_market');
-		    			break;
-		    		case 'declaraciones':
-		    			$webhook = config('discord.webhook_statements');
-		    			break;
-		    	}
-    		}
-	    	$category = $event->post->category;
-	    	$title = $event->post->title;
-	    	$description = $event->post->description;
-	    	if ($event->post->match_id) {
-	    		$link = route('match', $event->post->match_id);
-		        return Http::post($webhook, [
-		            'embeds' => [
-		                [
-		                    'title' => $title,
-		                    'description' => $description,
-		                    'url' => $link,
-		                    'color' => '7506394',
-		                ]
-		            ],
-		        ]);
-	    	} else {
-		        return Http::post($webhook, [
-		            // 'content' => $category,
-		            'embeds' => [
-		                [
-		                    'title' => $title,
-		                    'description' => $description,
-		                    'color' => '7506394',
-		                ]
-		            ],
-		        ]);
-	    	}
-    	}
+    	if ($this->activeNotifications) {
+            $webhook = $this->getWebhookUrl($event->post->type);
+            $post = $event->post;
+            $category = $post->category;
+            $title = $post->title;
+            $description = $post->description;
+
+            $data = [
+                'embeds' => [
+                    [
+                        'title' => $title,
+                        'description' => $description,
+                        'color' => '7506394',
+                    ]
+                ],
+            ];
+
+            if ($post->match_id) {
+                $data['embeds'][0]['url'] = route('match', $event->post->match_id);
+            }
+
+            $this->httpClient->request('POST', $webhook, [
+                'json' => $data,
+            ]);
+        }
+    }
+
+    private function getWebhookUrl($type): string
+    {
+        if ($this->testMode) {
+            return config('discord.webhook_dev');
+        }
+
+        switch ($type) {
+            case 'general':
+                return config('discord.webhook_general');
+            case 'resultados':
+            case 'rachas':
+            case 'destacados':
+                return config('discord.webhook_matches');
+            case 'records':
+                return config('discord.webhook_records');
+            case 'lesiones':
+                return config('discord.webhook_injuries');
+            case 'movimientos':
+                return config('discord.webhook_market');
+            case 'declaraciones':
+                return config('discord.webhook_statements');
+            default:
+                return config('discord.webhook_default');
+        }
     }
 }
